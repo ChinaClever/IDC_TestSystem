cd3560f50d089552197effb20276d2348e8e54a4
ChinaWang2016 <1175512939@qq.com>
2018-08-09 16:15:58 +0800

    更改界面

--- IDC_TestSystem/mpdu/m_logs/m_logenvwid.cpp
+++ IDC_TestSystem/mpdu/m_logs/m_logenvwid.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
  *
  *
  *  Created on: 2018年10月1日
@@ -33,7 +33,7 @@ void M_LogEnvWid::initTable()
     setGroupBoxTitle(mTableTile);
 
     mHeadList.clear();
-    mHeadList << tr("编号") << tr("日期") << tr("时间") << tr("设备号") << tr("传感顺")
+    mHeadList << tr("编号") << tr("日期") << tr("时间") << tr("设备号") << tr("传感器")
               << tr("温度") << tr("温度下限") << tr("温度上限")
               << tr("湿度") << tr("湿度下限") << tr("湿度上限") ;
     model->setHeaders(mHeadList);
