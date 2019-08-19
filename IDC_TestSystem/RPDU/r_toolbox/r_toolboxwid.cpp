57f8d6718ae356d293dfa91f114bd666f1b166fd
luozhiyong <luozhiyong131@qq.com>
2019-08-19 10:48:22 +0800

    解决问题

--- IDC_TestSystem/RPDU/r_toolbox/r_toolboxwid.cpp
+++ IDC_TestSystem/RPDU/r_toolbox/r_toolboxwid.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
  *
  *
  *  Created on: 2018年10月1日
@@ -8,10 +8,9 @@
 
 R_ToolBoxWid::R_ToolBoxWid(QWidget *parent) :  ToolBoxWid(parent)
 {
+    mConfig = R_ConfigFile::bulid();
     mModeToolWid = new R_ModeToolWid();
     mStatusToolWid = new R_StatusToolWid();
     mLogsToolWid = new R_LogsToolWid();
     mSettingToolWid = new R_SettingToolWid();
-    mConfig = R_ConfigFile::bulid();
-
 }
